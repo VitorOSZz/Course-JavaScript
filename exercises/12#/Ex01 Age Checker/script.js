8b12ab3727eb3743b190b2b304e543c16683eda6
VitorOSZz <harlerquin224@gmail.com>
2023-10-22 17:21:04 -0300

    Correcting Exercise with Class And GPT

--- exercises/12#/Ex01 Age Checker/script.js
+++ exercises/12#/Ex01 Age Checker/script.js
@@ -1,93 +1,93 @@
-YearBornElement = window.document.querySelector('#YearBorn')
-SexButtonMaleElement = window.document.querySelector('#SexButtonMale')
-SexButtonFemaleElement = window.document.querySelector('#SexButtonFemale')
-CheckButtonElement = window.document.querySelector('#CheckButton')
-ResultElement = window.document.querySelector('#Result')
-Result_imageElement = window.document.querySelector('#Result-Image')
+// Variables for DOM elements
+var YearBornElement = window.document.querySelector('#YearBorn');
+var CheckButtonElement = window.document.querySelector('#CheckButton');
+var ResultElement = window.document.querySelector('#Result');
+var ResultImageElement = window.document.querySelector('#Result-Image');
+var SexButtonElement = '';
 
-CheckButtonElement.addEventListener('click', clicked)
-SexButtonMaleElement.addEventListener('click', genderMale)
-SexButtonFemaleElement.addEventListener('click', genderFemale)
+// Add an event listener to the "Check" button
+CheckButtonElement.addEventListener('click', clicked);
 
-Data = new Date()
-NowYear = Data.getFullYear()
+// Get the current year
+var Date = new Date();
+var NowYear = Date.getFullYear();
 
-function genderMale () {
-    SexButtonElement = 'Male'
-    console.log('Male')
+// Function to check the selected sex
+function SexChecker() {
+    var Sex = window.document.getElementsByName('Sex');
+    if (Sex[0].checked) {
+        SexButtonElement = 'Male';
+        console.log('Sex: Male');
+    } else if (Sex[1].checked) {
+        SexButtonElement = 'Female';
+        console.log('Sex: Female');
+    } else {
+        ResultElement.innerHTML = 'Error in Sex';
+        console.log('Sex: Error');
+    }
 }
-function genderFemale () {
-    SexButtonElement = 'Female'
-    console.log('Female')
-}
-function clicked () {
-    YearBornElement = Number(YearBornElement.value)
-    Age = NowYear - YearBornElement
-    console.log(Age)
 
+// Function to handle button click
+function clicked() {
+    // Clear previous results
+    SexButtonElement = '';
+    ResultElement.innerHTML = '';
+    ResultImageElement.innerHTML = '';
 
-    switch (true){
+    // Check the selected sex
+    SexChecker();
 
-        case Age >=0 && Age < 4:
-            if (SexButtonElement == 'Male') {
-                ResultElement.innerHTML = `He have ${Age} years old`
-                Result_imageElement.innerHTML = `<img src="photos/baby/Baby Man.jpg" alt="">`
-            }
-            if (SexButtonElement == 'Female') {
-                ResultElement.innerHTML = `She have ${Age} years old`
-                Result_imageElement.innerHTML = `<img src="photos/baby/Baby Girl.jpg" alt="">`
-            }
-        break
+    // Get the year of birth and calculate the age
+    var YearBorn = Number(YearBornElement.value);
+    var Age = NowYear - YearBorn;
+    console.log(Age);
 
-        case Age >=4 && Age < 14:
-            if (SexButtonElement == 'Male') {
-                ResultElement.innerHTML = `He have ${Age} years old`
-                Result_imageElement.innerHTML = `<img src="photos/kid/Kid man.jpg" alt="">`
-            }
-            if (SexButtonElement == 'Female') {
-                ResultElement.innerHTML = `She have ${Age} years old`
-                Result_imageElement.innerHTML = `<img src="photos/kid/Kid Girl.jpeg" alt="">`
-            }
-        break
+    // Validate the year of birth
+    if (YearBorn > NowYear || YearBorn === 0) {
+        ResultElement.innerHTML = 'Error :/ your Year is incorrect';
+    } else {
+        // Based on the age, display appropriate information and images
+        switch (true) {
+            case Age >= 0 && Age < 4:
+                var ageLabel = SexButtonElement === 'Male' ? 'He' : 'She';
+                ResultElement.innerHTML = ageLabel + ' has ' + Age + ' years old';
+                var babyImage = SexButtonElement === 'Male' ? 'Baby Man.jpg' : 'Baby Girl.jpg';
+                ResultImageElement.innerHTML = '<img src="photos/baby/' + babyImage + '" alt="">';
+                break;
 
-        case Age >=14 && Age < 20:
-            if (SexButtonElement == 'Male') {
-                ResultElement.innerHTML = `He have ${Age} years old`
-                Result_imageElement.innerHTML = `<img src="photos/young/Young Man.jpg" alt="">`
-            }
-            if (SexButtonElement == 'Female') {
-                ResultElement.innerHTML = `She have ${Age} years old`
-                Result_imageElement.innerHTML = `<img src="photos/young/Young Girl.jpg" alt="">`
-            }
-        break
+            case Age >= 4 && Age < 14:
+                var kidLabel = SexButtonElement === 'Male' ? 'He' : 'She';
+                ResultElement.innerHTML = kidLabel + ' has ' + Age + ' years old';
+                var kidImage = SexButtonElement === 'Male' ? 'Kid Man.jpg' : 'Kid Girl.jpeg';
+                ResultImageElement.innerHTML = '<img src="photos/kid/' + kidImage + '" alt="">';
+                break;
 
-        case Age >=20 && Age < 60:
-            if (SexButtonElement == 'Male') {
-                ResultElement.innerHTML = `He have ${Age} years old`
-                Result_imageElement.innerHTML = `<img src="photos/adult/Adult Man.jpg" alt="">`
-            }
-            if (SexButtonElement == 'Female') {
-                ResultElement.innerHTML = `She have ${Age} years old`
-                Result_imageElement.innerHTML = `<img src="photos/adult/Adult Girl.jpeg" alt="">`
-            }
-        break
+            case Age >= 14 && Age < 20:
+                var youngLabel = SexButtonElement === 'Male' ? 'He' : 'She';
+                ResultElement.innerHTML = youngLabel + ' has ' + Age + ' years old';
+                var youngImage = SexButtonElement === 'Male' ? 'Young Man.jpg' : 'Young Girl.jpg';
+                ResultImageElement.innerHTML = '<img src="photos/young/' + youngImage + '" alt="">';
+                break;
 
-        case Age >=60:
-            if (SexButtonElement == 'Male') {
-                ResultElement.innerHTML = `He have ${Age} years old`
-                Result_imageElement.innerHTML = `<img src="photos/old/Old Man.jpg" alt="">`
-            }
-            if (SexButtonElement == 'Female') {
-                ResultElement.innerHTML = `She have ${Age} years old`
-                Result_imageElement.innerHTML = `<img src="photos/old/Old Woman.jpg" alt="">`
-            }
-        break
+            case Age >= 20 && Age < 60:
+                var adultLabel = SexButtonElement === 'Male' ? 'He' : 'She';
+                ResultElement.innerHTML = adultLabel + ' has ' + Age + ' years old';
+                var adultImage = SexButtonElement === 'Male' ? 'Adult Man.jpg' : 'Adult Girl.jpeg';
+                ResultImageElement.innerHTML = '<img src="photos/adult/' + adultImage + '" alt="">';
+                break;
 
-        default:
-            ResultElement.innerHTML = 'Error :/'
-            break
+            case Age >= 60:
+                var oldLabel = SexButtonElement === 'Male' ? 'He' : 'She';
+                ResultElement.innerHTML = oldLabel + ' has ' + Age + ' years old';
+                var oldImage = SexButtonElement === 'Male' ? 'Old Man.jpg' : 'Old Woman.jpg';
+                ResultImageElement.innerHTML = '<img src="photos/old/' + oldImage + '" alt="">';
+                break;
+
+            default:
+                ResultElement.innerHTML = 'Error :/';
+                break;
+        }
     }
 
-
-    console.log(NowYear)
+    console.log(NowYear);
 }
