7c951fa48915fc301b42a627b0f31c7ff49beeaa
VitorOSZz <harlerquin224@gmail.com>
2023-10-22 18:05:56 -0300

    New system of img :D

--- exercises/12#/Ex01 Age Checker/script.js
+++ exercises/12#/Ex01 Age Checker/script.js
@@ -1,93 +1,114 @@
 // Variables for DOM elements
-var YearBornElement = window.document.querySelector('#YearBorn');
-var CheckButtonElement = window.document.querySelector('#CheckButton');
-var ResultElement = window.document.querySelector('#Result');
-var ResultImageElement = window.document.querySelector('#Result-Image');
-var SexButtonElement = '';
+var img = window.document.createElement('img')
+var YearBornElement = window.document.querySelector('#YearBorn')
+var CheckButtonElement = window.document.querySelector('#CheckButton')
+var ResultElement = window.document.querySelector('#Result')
+var Result_imageElement = window.document.querySelector('#Result-Image')
+var SexButtonElement = ''
 
 // Add an event listener to the "Check" button
-CheckButtonElement.addEventListener('click', clicked);
+CheckButtonElement.addEventListener('click', clicked)
 
 // Get the current year
-var Date = new Date();
-var NowYear = Date.getFullYear();
+var Date = new Date()
+var NowYear = Date.getFullYear()
 
 // Function to check the selected sex
 function SexChecker() {
-    var Sex = window.document.getElementsByName('Sex');
+    var Sex = window.document.getElementsByName('Sex')
     if (Sex[0].checked) {
-        SexButtonElement = 'Male';
-        console.log('Sex: Male');
+        SexButtonElement = 'Male'
+        console.log('Sex: Male')
     } else if (Sex[1].checked) {
-        SexButtonElement = 'Female';
-        console.log('Sex: Female');
+        SexButtonElement = 'Female'
+        console.log('Sex: Female')
     } else {
-        ResultElement.innerHTML = 'Error in Sex';
-        console.log('Sex: Error');
+        ResultElement.innerHTML = 'Error in Sex'
+        console.log('Sex: Error')
     }
 }
 
 // Function to handle button click
 function clicked() {
+
     // Clear previous results
-    SexButtonElement = '';
-    ResultElement.innerHTML = '';
-    ResultImageElement.innerHTML = '';
+    SexButtonElement = ''
+    ResultElement.innerHTML = ''
+    Result_imageElement.innerHTML = ''
 
     // Check the selected sex
-    SexChecker();
+    SexChecker()
 
     // Get the year of birth and calculate the age
-    var YearBorn = Number(YearBornElement.value);
-    var Age = NowYear - YearBorn;
-    console.log(Age);
+    var YearBorn = Number(YearBornElement.value)
+    var Age = NowYear - YearBorn
+    console.log(Age)
 
     // Validate the year of birth
-    if (YearBorn > NowYear || YearBorn === 0) {
-        ResultElement.innerHTML = 'Error :/ your Year is incorrect';
-    } else {
-        // Based on the age, display appropriate information and images
+    if (YearBorn > NowYear || YearBorn == 0) {
+        img.setAttribute('src', 'https://cdn.oantagonista.com/cdn-cgi/image/fit=contain,width=960,format=auto/uploads/2023/06/Jair-Bolsonaro-29.jun_.2023-scaled.jpg')
+        ResultElement.innerHTML = 'Error :/ your Year is incorrect'
+    }
+    else {
         switch (true) {
             case Age >= 0 && Age < 4:
-                var ageLabel = SexButtonElement === 'Male' ? 'He' : 'She';
-                ResultElement.innerHTML = ageLabel + ' has ' + Age + ' years old';
-                var babyImage = SexButtonElement === 'Male' ? 'Baby Man.jpg' : 'Baby Girl.jpg';
-                ResultImageElement.innerHTML = '<img src="photos/baby/' + babyImage + '" alt="">';
-                break;
+                if (SexButtonElement == 'Male') {
+                    ResultElement.innerHTML = `He has ${Age} years old`
+                    img.setAttribute('src', 'photos/baby/baby man.jpg')
+                } else if (SexButtonElement == 'Female') {
+                    ResultElement.innerHTML = `She has ${Age} years old`
+                    img.setAttribute('src', 'photos/baby/Baby girl.jpg')
+                }
+                break
 
             case Age >= 4 && Age < 14:
-                var kidLabel = SexButtonElement === 'Male' ? 'He' : 'She';
-                ResultElement.innerHTML = kidLabel + ' has ' + Age + ' years old';
-                var kidImage = SexButtonElement === 'Male' ? 'Kid Man.jpg' : 'Kid Girl.jpeg';
-                ResultImageElement.innerHTML = '<img src="photos/kid/' + kidImage + '" alt="">';
-                break;
+                if (SexButtonElement == 'Male') {
+                    ResultElement.innerHTML = `He has ${Age} years old`
+                    img.setAttribute('src', 'photos/kid/Kid man.jpg')
+                } else if (SexButtonElement == 'Female') {
+                    ResultElement.innerHTML = `She has ${Age} years old`
+                    img.setAttribute('src', 'photos/kid/Kid Girl.jpeg')
+                }
+                break
 
             case Age >= 14 && Age < 20:
-                var youngLabel = SexButtonElement === 'Male' ? 'He' : 'She';
-                ResultElement.innerHTML = youngLabel + ' has ' + Age + ' years old';
-                var youngImage = SexButtonElement === 'Male' ? 'Young Man.jpg' : 'Young Girl.jpg';
-                ResultImageElement.innerHTML = '<img src="photos/young/' + youngImage + '" alt="">';
-                break;
+                if (SexButtonElement == 'Male') {
+                    ResultElement.innerHTML = `He has ${Age} years old`
+                    img.setAttribute('src', 'photos/young/Young Man.jpg')
+                } else if (SexButtonElement == 'Female') {
+                    ResultElement.innerHTML = `She has ${Age} years old`
+                    img.setAttribute('src', 'photos/young/Young Girl.jpg')
+                }
+                break
 
             case Age >= 20 && Age < 60:
-                var adultLabel = SexButtonElement === 'Male' ? 'He' : 'She';
-                ResultElement.innerHTML = adultLabel + ' has ' + Age + ' years old';
-                var adultImage = SexButtonElement === 'Male' ? 'Adult Man.jpg' : 'Adult Girl.jpeg';
-                ResultImageElement.innerHTML = '<img src="photos/adult/' + adultImage + '" alt="">';
-                break;
+                if (SexButtonElement == 'Male') {
+                    ResultElement.innerHTML = `He has ${Age} years old`
+                    img.setAttribute('src', 'photos/adult/Adult Man.jpg')
+                } else if (SexButtonElement == 'Female') {
+                    ResultElement.innerHTML = `She has ${Age} years old`
+                    img.setAttribute('src', 'photos/adult/Adult Girl.jpeg')
+                }
+                break
 
             case Age >= 60:
-                var oldLabel = SexButtonElement === 'Male' ? 'He' : 'She';
-                ResultElement.innerHTML = oldLabel + ' has ' + Age + ' years old';
-                var oldImage = SexButtonElement === 'Male' ? 'Old Man.jpg' : 'Old Woman.jpg';
-                ResultImageElement.innerHTML = '<img src="photos/old/' + oldImage + '" alt="">';
-                break;
+                if (SexButtonElement == 'Male') {
+                    ResultElement.innerHTML = `He has ${Age} years old`
+                    img.setAttribute('src', 'photos/old/Old Man.jpg')
+                } else if (SexButtonElement == 'Female') {
+                    ResultElement.innerHTML = `She has ${Age} years old`
+                    img.setAttribute('src', 'photos/old/Old Woman.jpg')
+                }
+                break
 
             default:
-                ResultElement.innerHTML = 'Error :/';
-                break;
+                SexButtonElement = ''
+                img.setAttribute('src', 'https://cdn.oantagonista.com/cdn-cgi/image/fit=contain,width=960,format=auto/uploads/2023/06/Jair-Bolsonaro-29.jun_.2023-scaled.jpg')
+                ResultElement.innerHTML = 'Error :/'
+                break
         }
     }
+    Result_imageElement.appendChild(img)
 
-    console.log(NowYear);
+    console.log(NowYear)
 }
