8e706890612c2e654d5012be330f5f852a22ea82
VitorOSZz <harlerquin224@gmail.com>
2023-10-23 17:28:59 -0300

    14# for(x; x>=y; c++

---
+++ class/14# For(x ,y ,x++)/script.js
@@ -0,0 +1,12 @@
+howmany = window.prompt('Say a Number')
+howmany = Number(howmany)
+/*
+c= 0
+
+do {
+    c++
+    console.log(c)
+}while (c < howmany) */
+for(c=0; c <= howmany; c++) {
+    console.log(c)
+}
