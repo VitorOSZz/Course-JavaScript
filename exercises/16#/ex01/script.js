df42971bcc197a3661584477c2cd7ff668d4201e
VitorOSZz <harlerquin224@gmail.com>
2023-10-25 09:42:51 -0300

    Correct bugs and now can use with negative numbers

--- exercises/16#/ex01/script.js
+++ exercises/16#/ex01/script.js
@@ -1,44 +1,68 @@
-var Counted = 1
-var ResultElement = window.document.querySelector('#Result')
+var ResultElement = window.document.querySelector('#Result');
+var Result2Element = window.document.querySelector('#Result2');
 
-var Result2Elemnet = window.document.createElement('p')
-function Count () {
+function Count() {
     // Get all values
-    var StartNumber = window.document.querySelector('#StartNumber')
-    var StartNumber = Number(StartNumber.value)
-    // 
-    var EndNumber = window.document.querySelector('#EndNumber')
-    var EndNumber = Number(EndNumber.value)
-    //
-    var JumpsNumber = window.document.querySelector('#JumpsNumber')
-    var JumpsNumber = Number(JumpsNumber.value)
-    //
-    console.log(StartNumber, EndNumber, JumpsNumber)
+    var StartNumber = window.document.querySelector('#StartNumber');
+    StartNumber = Number(StartNumber.value);
     
-    // End- Start = X
-    // X / JumpsNumber
+    var EndNumber = window.document.querySelector('#EndNumber');
+    EndNumber = Number(EndNumber.value);
+    
+    var JumpsNumber = window.document.querySelector('#JumpsNumber');
 
-    End_Minus_Start = EndNumber - StartNumber
-    HowMany_Counts = End_Minus_Start / JumpsNumber
-    RealNumber = StartNumber
+    // End - Start = X
+    // X / JumpsNumber
+    if (JumpsNumber.value !== '' && JumpsNumber.value !== '0') {
+        JumpsNumber = Number(JumpsNumber.value);
+
+        console.log(StartNumber, EndNumber, JumpsNumber);
+
+        var End_Minus_Start = EndNumber - StartNumber;
+        var HowMany_Counts = End_Minus_Start / JumpsNumber;
+        var RealNumber = StartNumber;
+    } else {
+        console.log('Error')
+        ResultElement.innerHTML = 'Error, say a Jump Number!'
+        Count.stop()
+    }
 
     /*while (Counted <= HowMany_Counts) {
-        Counted+++
-        console.log(`Real Number: ${RealNumber = RealNumber+JumpsNumber}`)
+        Counted++;
+        console.log(`Real Number: ${RealNumber = RealNumber + JumpsNumber}`);
     }*/
 
-    var resultString = ""
+    var resultString = "";
 
-    for(Counted= 0; Counted <= HowMany_Counts; Counted++) {
-        console.log (RealNumber)
-        resultString += RealNumber + " "
-        RealNumber = RealNumber+JumpsNumber
+    if (StartNumber < EndNumber) {
+        for (Counted = 0; Counted <= HowMany_Counts; Counted++) {
+            console.log(RealNumber);
+            resultString += RealNumber + " 👉 ";
+            RealNumber = RealNumber + JumpsNumber;
 
-        if (RealNumber > EndNumber) {
-            RealNumber = RealNumber - JumpsNumber
-            console.log (`Real Real Number: ${RealNumber}`)
+            if (RealNumber > EndNumber) {
+                RealNumber = RealNumber - JumpsNumber;
+                console.log(`Real Real Number: ${RealNumber}`);
+            }
+        }
+    } else if (StartNumber > EndNumber) {
+
+        console.log('Start > End')
+
+        var End_Minus_Start = StartNumber - EndNumber;
+        var HowMany_Counts = End_Minus_Start / JumpsNumber;
+        var RealNumber = StartNumber;
+
+        for (Counted = 0; Counted <= HowMany_Counts; Counted++) {
+            console.log(RealNumber);
+            resultString += RealNumber + " 👉 ";
+            RealNumber = RealNumber - JumpsNumber;
         }
     }
-    ResultElement.innerHTML = resultString
+    if ( StartNumber == EndNumber) {
+        ResultElement.innerHTML = ' Error please dont say the same number';
+    } else if (StartNumber !== EndNumber ) {
+        ResultElement.innerHTML = resultString + "🏴";
+    }
 
 }
